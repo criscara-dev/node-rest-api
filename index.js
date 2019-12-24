b239bfd33a1e22e0a235a9266b95173afe5b6678
Cristian Caratti <ccristian.caratti.xx@gmail.com>
2019-12-24 14:01:42 +0000

    finished and tested users checks service API

--- index.js
+++ index.js
@@ -60,7 +60,8 @@ httpsServer.listen(config.httpsPort, function() {
 const router = {
   ping: handlers.ping,
   users: handlers.users,
-  tokens: handlers.tokens
+  tokens: handlers.tokens,
+  checks: handlers.checks
 };
 
 // Unified server: all the server logic for both http and https createServer
